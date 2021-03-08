0cfc6bc177968314e55819841127ca10f0089eb1
Rey Jesus Teves <jesus.reyteves@gmail.com>
2021-03-08 19:37:29 +0800

    updated jumbo component

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -6,9 +6,9 @@ import Button from 'react-bootstrap/Button'
 export default function Jumbo() {
     return (
         <Jumbotron>
-            <h1>Hello, world!</h1>
-            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <Button variant="primary">Learn more</Button>
+            <h1>Batch 101 - Hello, world!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ullam nobis minima assumenda ad quam natus vitae, eveniet alias dolorum tempore sapiente sint, possimus, porro, dicta! Iure placeat, at ratione!</p>.</p>
+            <Button variant="primary">Blog now</Button>
         </Jumbotron>
     )
 }
